b8542b2ec3996da4c6df6a8e5050ea1df6a0e723
javekk <periniraffaele@gmail.com>
2020-03-29 14:03:43 +0200

    Make me fancy

--- main.py
+++ main.py
@@ -20,14 +20,13 @@ from pymongo import MongoClient
 from bson.objectid import ObjectId 
 
 # Instantiate mongoDB client object, from that we get our db, and in turn the right collection 
-client = MongoClient('mongodb://localhost:27017/')
+client = MongoClient('mongodb://mongodb:27017/')
 db = client.vehicles
 cars = db.cars
 
 # Make the Flask Service
 app = Flask(__name__)
 
-id = '-1'
 
 # Implementaion for the root URL, on http://localhost:5000/ 
 @app.route('/')
@@ -38,18 +37,21 @@ def index():
 @app.route('/cars', methods = ['GET', 'POST', 'PUT', 'DELETE'])
 def user():
 
+    id = '-1'
+
     # Check if there is the id in the parameters
     if 'id' in request.args: # get id
         id = request.args.get('id')
 
     if request.method == 'GET':
-        if 'id' != '-1':
+        if id != '-1':
         # http://localhost:5000/cars?id=<id>, return a specif car
             id = request.args.get('id')
             return str(cars.find_one({"_id": ObjectId(id)}))
         else: 
         # http://localhost:5000/cars, return every car in the collection
             return ''.join([str(car) for car in cars.find()])
+            
 
     if request.method == 'POST':
         # http://localhost:5000/cars, post a car, return the new id
@@ -72,4 +74,4 @@ def user():
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0')
